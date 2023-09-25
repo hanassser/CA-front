374a8258c6cd7a4f38a713dd0272772f86c18a20
Hang <hanassser@gmail.com>
2023-09-25 22:08:10 +0200

    feat: add home-course page

---
+++ src/pages/services/home-course/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import {
+  Form,
+  Spin,
+} from "antd";
+import MyPagination, { PageInfo } from "@/components/pagination";
+import { getCourse} from "@/api/service";
+
+import "./index.less";
+import {CourseList} from "@/types"
+
+import CardShow from "@/components/card/card";
+
+
+export default function SearchPage() {
+  const [searchForm] = Form.useForm();
+  const [pageData, setPageData] = useState<PageInfo>({ page: 1 });
+  const [cardData, setData] = useState<CourseList>([]);
+  const [load, setLoad] = useState(true);
+  const [total, setTotal] = useState(0);
+
+  const getDataList = (data: PageInfo) => {
+    getCourse(data).then((res) => {
+      const { data, status } = res;
+      if (status === 0 && data) {
+        let { list, total } = data;
+        setTotal(total);
+        setData(list);
+        setLoad(false);
+        return;
+      }
+    });
+  };
+
+  const pageChange = (pageData: PageInfo) => {
+    let data = searchForm.getFieldsValue();
+    getDataList({ ...pageData, ...data });
+    setPageData(pageData);
+  };
+
+
+  return (
+      <div className="container">
+        <Spin spinning={load}>
+          <CardShow data={cardData}/>
+          <MyPagination
+              page={pageData.page}
+              immediately={getDataList}
+              change={pageChange}
+              total={total}
+          />
+
+        </Spin>
+      </div>
+  );
+}
+SearchPage.route = {
+  [MENU_PATH]: "/list/course",
+};
