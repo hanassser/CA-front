577838ddb1ae6f8289f2eacb3bcc4a74599752cc
Hang <hanassser@gmail.com>
2023-09-27 22:47:04 +0200

    feat: add service detail page

---
+++ src/pages/services/service-detail/index.tsx
@@ -0,0 +1,20 @@
+import "./index.less";
+import React, { useState } from 'react';
+
+
+
+
+export default function ServiceDetailPage() {
+
+
+    return (
+        <div className="container">
+
+
+
+        </div>
+    );
+}
+ServiceDetailPage.route = {
+    [MENU_PATH]: "/list/service-detail",
+};
