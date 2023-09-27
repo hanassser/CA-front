151148cd7aa2a8528b466ac1d4d23607f87ec0b6
Hang <hanassser@gmail.com>
2023-09-27 22:36:00 +0200

    feat: add service nav link

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,6 +1,6 @@
 import { Card, Avatar, Row, Col, Typography,  } from "antd";
-import "./index.less";
 import {EventList} from "@/types";
+import {Link} from "react-router-dom";
 
 
 const { Meta } = Card;
@@ -13,32 +13,33 @@ export default function CardShow({data} :CardShowProps) {
     const cardStyle = {
         width: 340,
     };
-  return (
-    <div className="card-container">
-      <Row gutter={[16, 16]}>
+    return (
+        <div className="card-container">
+            <Row gutter={[16, 16]}>
+                {data.map((item:any) => (
+                    <Col span={8} key={item.name}>
+                        <Link to={`/list/card/${item.m_id}`}>
+                            <Card
+                                hoverable
+                                style={ cardStyle }
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                            >
+                                <Meta
+                                    title={item.name}
+                                    description={
+                                        <Typography.Paragraph ellipsis={{ rows: 12, suffix: "..." }}>
+                                            {item.price}
+                                        </Typography.Paragraph>
+                                    }
+                                />
+                                <p>{item.description}</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
 
-        {data.map((item:any) => (
-          <Col span={8} key={item.name}>
-            <Card
-              hoverable
-              style={ cardStyle }
-              cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-            >
-              <Meta
-                title={item.name}
-                description={
-                  <Typography.Paragraph ellipsis={{ rows: 12, suffix: "..." }}>
-                    {item.price}
-                  </Typography.Paragraph>
-                }
-              />
-                <p>{item.description}</p>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-
-    </div>
-  );
+        </div>
+    );
 }
 CardShow.route = { [MENU_PATH]: "/list/card" }
